e2902d64228f4888fa9a6e7360a2993bccc27123
Giovanni Piñeros <gjpinerosm@gmail.com>
2017-03-26 20:06:05 -0500

    Proyeco inicial se lectura de archivo .xls

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Autor: Giovanni Piñeros Mora
+# DAte: 26/03/2017
 # Excel-read-files
+# Comments:
+# Se realiza la lectura de archivos xls
+#
---
+++ readFilesXls/src/main/java/LecturaArchivosExcelXLS/LeerExcel.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * Created by GiovanniJavierRicard on 26/03/2017.
+ */
+public class LeerExcel {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream archivo = new FileInputStream(new File(""))
+    }
+}
